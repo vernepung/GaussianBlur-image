373a298fcf490cb3e3990937358157e9a461092a
yangguang.luo <yanggunag.luo@ivydad.com>
2016-06-13 14:57:28 +0800

    Initial Commit

---
+++ GaussianBlur-image/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GaussianBlur-image
+//
+//  Created by 骆阳光iMac on 16/6/13.
+//  Copyright © 2016年 骆阳光iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GaussianBlur-image/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GaussianBlur-image
+//
+//  Created by 骆阳光iMac on 16/6/13.
+//  Copyright © 2016年 骆阳光iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
